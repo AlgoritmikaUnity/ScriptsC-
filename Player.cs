6531ca6430453ca87208a3e6f04ee4cc8d3cb551
AlgoritmikaUnity <165564216+AlgoritmikaUnity@users.noreply.github.com>
2024-06-04 13:07:12 +0500

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -2,28 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Player : MonoBehaviour
 {
-
-    private int health = 10;
-    public GameObject fireball;
-    public Transform attackPoint;
-    public void TakeDamage(int damage)
+    public static List<Square> squares;
+    public static int score = 0;
+    private void Awake()
     {
-        health -= damage;
-        if(health < 0)
-        {
-            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        }
+        squares = new List<Square>();
     }
-    private void Update()
+     
+    void Update()
     {
-        if (Input.GetMouseButtonDown(0)) ;
-        Instantiate(fireball, attackPoint.position, attackPoint.rotation);
+      if (squares.Count == 0)
+      {
+            Victory();
+      } 
     }
-}
-
-internal class damage
-{
+public static void Defeat()
+    {
+        score = 0;
+        UI.ShowDefeatPanel();
+    }
+    public static void Victory()
+    {
+        UI.ShowVictoryPanel();
+    }
+    public static void Restart()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index);
+    }
 }
